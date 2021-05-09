7db3bbfbaa3238ef14e6c904e008a44928d9490f
Steve Gildersleeve <steve@swamppoodlesoftware.com>
2021-05-09 11:17:17 -0400

    Using material-ui and Roboto

--- web/banner/src/root.component.js
+++ web/banner/src/root.component.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 export default function Root(props) {
   return (
     <div>
       <section>{props.name} is mounted!</section>
       <hr />
-      <span>Home</span>
+      <Button variant="contained">Hello World</Button>
       <span>Next</span>
     </div>
   );
