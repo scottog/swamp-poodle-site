8609f55bdc2b0c5b9e800c30b6f5ee83ce766ac3
Steve Gildersleeve <steve@swamppoodlesoftware.com>
2021-05-09 17:51:46 -0400

    Update prettier for husky auto-linting v2

---
+++ web/main/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+}
