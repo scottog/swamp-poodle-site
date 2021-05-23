4d745d38a2645daf66b4b4a9fe1d2d1c9e898449
Steve Gildersleeve <steve@swamppoodlesoftware.com>
2021-05-23 01:22:13 -0400

    Source materials to mod

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,50 +7,52 @@
 // Scripts
 //
 
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
+  // From Business-Casual
+  const listHoursArray = document.body.querySelectorAll('.list-hours li')
+  listHoursArray[new Date().getDay()].classList.add('today')
+
   // Navbar shrink function
   var navbarShrink = function () {
-    const navbarCollapsible = document.body.querySelector("#mainNav");
+    const navbarCollapsible = document.body.querySelector('#mainNav')
     if (!navbarCollapsible) {
-      return;
+      return
     }
     if (window.scrollY === 0) {
-      navbarCollapsible.classList.remove("navbar-shrink");
+      navbarCollapsible.classList.remove('navbar-shrink')
     } else {
-      navbarCollapsible.classList.add("navbar-shrink");
+      navbarCollapsible.classList.add('navbar-shrink')
     }
-  };
+  }
 
   // Shrink the navbar
-  navbarShrink();
+  navbarShrink()
 
   // Shrink the navbar when page is scrolled
-  document.addEventListener("scroll", navbarShrink);
+  document.addEventListener('scroll', navbarShrink)
 
   // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector("#mainNav");
+  const mainNav = document.body.querySelector('#mainNav')
   if (mainNav) {
     new bootstrap.ScrollSpy(document.body, {
-      target: "#mainNav",
+      target: '#mainNav',
       offset: 74,
-    });
+    })
   }
 
   // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector(".navbar-toggler");
-  const responsiveNavItems = [].slice.call(
-    document.querySelectorAll("#navbarResponsive .nav-link")
-  );
+  const navbarToggler = document.body.querySelector('.navbar-toggler')
+  const responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'))
   responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener("click", () => {
-      if (window.getComputedStyle(navbarToggler).display !== "none") {
-        navbarToggler.click();
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click()
       }
-    });
-  });
+    })
+  })
 
   // Activate SimpleLightbox plugin for portfolio items
   new SimpleLightbox({
-    elements: "#portfolio a.portfolio-box",
-  });
-});
+    elements: '#portfolio a.portfolio-box',
+  })
+})
